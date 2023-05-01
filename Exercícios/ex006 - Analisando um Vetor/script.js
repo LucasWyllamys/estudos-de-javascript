e32ffe3cf33649c9f4ffa93ce9957659300e80c2
Wyllamys <lucas.wyllamys@outlook.com>
2023-04-30 23:24:40 -0300

    30/04/2023

--- Exercícios/ex006 - Analisando um Vetor/script.js
+++ Exercícios/ex006 - Analisando um Vetor/script.js
@@ -1,8 +1,23 @@
-function adicionar() {
-    var txt_num = document.getElementById("num").value
-    window.alert(txt_num)
+var vet = []
+function adicionar() {   
+    const select = document.getElementById('addnum')        //Atribui o elemento DOM select a variável select.    
+    const txt_num = document.getElementById("num").value    //Atribui o valor do elemento DOM input number a variável txt_num.
+    const num = Number(txt_num)                             //Converte em número.
+    if(vet.indexOf(num) == -1) {
+        var item = document.createElement('option')         //Cria um elemento DOM option.
+        item.text = `Valor ${txt_num} adicionado.`          //Escreve no texto da select.
+        item.value = `select ${num}`                        //Adiciona um valor id a select.
+        select.appendChild(item)                            //Adiciona o item a select.
+        vet.push(num)                                       //Adiciona um elemento no vetor.
+    } else {
+        window.alert('Valor é inválido ou já encontrado na lista.')       
+    }
+    
+    //for(i = 0; i <= vet.length; i++) {
+        
+    //}
 }
 
-function finalizar() {
-    window.alert("finalizar")
-}
+//function finalizar() {
+
+//}
