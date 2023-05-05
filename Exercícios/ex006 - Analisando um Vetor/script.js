516eb4b4725670fcf00f8229608242786388d27f
Wyllamys <lucas.wyllamys@outlook.com>
2023-05-05 01:48:08 -0300

    05/05/2023

--- Exercícios/ex006 - Analisando um Vetor/script.js
+++ Exercícios/ex006 - Analisando um Vetor/script.js
@@ -1,18 +1,39 @@
 var vet = []
 const result = document.getElementById("result")
+const select = document.getElementById('addnum')        //Atribui o elemento DOM select a variável select.    
+
+function entre_1_100(n) {   //Verifica se o número está entre 1 e 100.
+    if(n >= 1 && n <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function na_lista(n, list) {    //Verifica se está na lista do vetor.
+    if(list.indexOf(n) == -1) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function limpar_div() {     //Limpa a div caso esteja com conteúdo.
+    if(result.innerHTML != null) {
+        result.innerHTML = null
+    }
+}
+
 function adicionar() {   
-    const select = document.getElementById('addnum')        //Atribui o elemento DOM select a variável select.    
     const txt_num = document.getElementById("num").value    //Atribui o valor do elemento DOM input number a variável txt_num.
     const num = Number(txt_num)                             //Converte em número.
-    if(vet.indexOf(num) == -1 && num >= 1 && num <= 100) {
-        if(result.innerHTML != null) {
-            result.innerHTML = null
-        }
+    if(!na_lista(num, vet) && entre_1_100(num)) {
+        limpar_div()                                        //Invoca a função limpar_div.
         var item = document.createElement('option')         //Cria um elemento DOM option.
         item.text = `Valor ${txt_num} adicionado.`          //Escreve no texto da select.
-        item.value = `select ${num}`                        //Adiciona um valor id a select.
+        item.value = `select ${num}`                        //Adiciona o valor/id a select.
         select.appendChild(item)                            //Adiciona o item a select.
-        vet.push(num)                                       //Adiciona um elemento no vetor.
+        vet.push(num)                                       //Adiciona o elemento ao vetor.
     } else {
         window.alert('Valor é inválido ou já encontrado na lista.')     //Exibe mensagem de erro.       
     }
