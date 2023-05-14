90238f3a6a3697840cc61e941c5d95d787302a96
Wyllamys <lucas.wyllamys@outlook.com>
2023-05-13 23:20:12 -0300

    13/05/2023

---
+++ Exercícios/Bloco 1/ex010 - Usando um JavaScript externo/script.js
@@ -0,0 +1,17 @@
+var resp = document.querySelector('p#resp')
+
+function clicoub1() {
+    resp.innerHTML += '<br><br>Você clicou no botão 1'
+}
+
+function clicoub2() {
+    resp.innerHTML += '<br><br>Você clicou no botão 2'
+}
+
+function clicoub3() {
+    resp.innerHTML += '<br><br>Você clicou no botão 3'
+}
+
+function clicoub4() {
+    resp.innerHTML += '<br><br>Você clicou no botão 4'
+}
